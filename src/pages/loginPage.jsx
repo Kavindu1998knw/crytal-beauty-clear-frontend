abb39588500127fa8d3a8d29d8a1f904c2ea9a6a
Kavindu1998knw <kavinduknw@gmail.com>
2025-05-28 21:41:50 -0700

    login page complete

---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,14 @@
+export default function LoginPage() {
+  return (
+    <div className="w-full h-screen bg-[url('./login-bg.jpg')] bg-cover bg-center flex">
+      <div className="w-1/2 h-full"></div>
+      <div className="w-1/2 h-full flex justify-center items-center">
+        <div className="w-[500px] h-[500px] backdrop-blur-sm rounded-xl flex flex-col justify-center items-center">
+            <input type="email" name="email" id="email" placeholder="Email" className="w-[400px] h-[50px] rounded-xl border-2 border-gray-300 m-2 p-2" />
+            <input type="password" name="password" id="password" placeholder="Password" className="w-[400px] h-[50px] rounded-xl border-2 border-gray-300 m-2 p-2" />
+            <button className="w-[400px] h-[50px] rounded-xl bg-blue-500 text-white m-2 p-2">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
